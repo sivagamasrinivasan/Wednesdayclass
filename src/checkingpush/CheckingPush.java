a4b4fb1bc01147fbd62b2f45b5fd64988dbc9a72
sivagamasrinivasan <784108@pdsb.net>
2024-01-31 08:27:20 -0500

    test

---
+++ src/checkingpush/CheckingPush.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package checkingpush;
+
+/**
+ *
+ * @author Sivagama
+ */
+public class CheckingPush {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       // push operation checking
+       // initializing git, .gitconfig, .gitignore , commit then push 
+    }
+
+}
